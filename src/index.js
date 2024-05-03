edeb3c36ca0bef7ecea825c00a0e9fc52528e0a7
AnmolMehta <anmolmehta1213@gmail.com>
2024-05-04 00:30:25 +0530

    Fetch weather forcast code implemented

--- src/index.js
+++ src/index.js
@@ -2,4 +2,41 @@ import { compareAsc, format } from "date-fns";
 import './styles/index.css';
 
 
-const button = document.querySelector('.btn');
+ 
+
+
+
+document.querySelector('.search').addEventListener('click', onButtonClick);
+
+async function onButtonClick(){  
+    const input = document.querySelector('.input')  
+    try{
+        const forecast = await fetchWeatherForecast(input.value);
+        displayForeCast(forecast);
+    }catch(error){
+        console.log(`ERROR: ${error}`)
+    }
+}
+function displayForeCast(futureWeather){
+    console.log(futureWeather)
+    futureWeather.forecast.forecastday.forEach(day =>{
+        console.log(day.date);
+    })
+
+    
+}
+   
+
+function fetchWeatherForecast(location){
+    return new Promise((resolve , reject) =>{
+        fetch(`http://api.weatherapi.com/v1/forecast.json?key=7fe00839801248baa93112333240105&q=${location}&days=3&aqi=yes&alerts=yes`,{mode : 'cors'})
+        .then(response => {
+            if(!response.ok){
+               return reject('Invalid location');
+            }    
+            resolve(response.json());
+        })
+        .then(response => resolve(response))
+        .catch(error => reject(error));
+    })
+}
