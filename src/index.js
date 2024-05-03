3684117590a6d9570b8d5a0e926f3e4c3ee4119b
AnmolMehta <anmolmehta1213@gmail.com>
2024-05-03 17:45:37 +0530

    Added starting files

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 import { compareAsc, format } from "date-fns";
 import './styles/index.css';
-import { startingHTML } from "./pages/loadHTML.js";
-import { projects, displayProjects} from "./pages/projects.js";
 
-startingHTML();
 
-const addNewProject = document.querySelector(".add-new-project-button-js")
-addNewProject.addEventListener("click", () =>{
-    const newProjectName = document.querySelector(".new-project-input-js").value
-    if(newProjectName !== ""){
-        projects.push(newProjectName);
-        displayProjects();
-    }
-    document.querySelector(".new-project-input-js").innerHTML = "";
-})
+const button = document.querySelector('.btn');
